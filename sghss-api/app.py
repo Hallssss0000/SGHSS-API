53fb6fcc6ae6966d4882ae27aa1f2b658a1c3d60
Hallssss0000 <90478770+Hallssss0000@users.noreply.github.com>
2026-02-10 01:33:24 -0300

    app.py

--- sghss-api/app.py
+++ sghss-api/app.py
@@ -98,4 +98,5 @@ if __name__ == '__main__':
     print("\nDocumentação completa: http://localhost:5000/health")
     print("=" * 50)
     
-    app.run(debug=True, port=5000, host='0.0.0.0')
+
+    app.run(port=5000, host='0.0.0.0')
